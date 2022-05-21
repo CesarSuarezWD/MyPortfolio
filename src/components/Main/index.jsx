65e50b00fd09d21fbf25a27b017ee3ff3ccd3993
CesarSuarezWD <cesarsuarezmedina@gmail.com>
2022-05-20 19:28:01 -0500

    GPT3 project added

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -120,6 +120,19 @@ function Main (){
 
                         <div className="column column--50">
                             <div className="work__content">
+                                <a href="https://cesarsuarezwd.github.io/GPT3/" target="_blank" rel="noreferrer">
+                                    <div className="gradient__overlay">
+                                        <img src="https://i.imgur.com/E7I9AkG.png"  alt="GPT3" className="img__work" />
+                                        <div className="work__overlay"></div>
+                                        <h3 className="title__product">GPT3</h3>
+                                        <div className="look__page">React / CSS<br></br><br></br>See the original page &#8658;</div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="column column--50">
+                            <div className="work__content">
                                 <a href="https://cesarsuarezwd.github.io/todo-machine/" target="_blank" rel="noreferrer">
                                     <div className="gradient__overlay">
                                         <img src="https://i.imgur.com/xy2YDdZ.png"  alt="TodoMachine pic" className="img__work" />
